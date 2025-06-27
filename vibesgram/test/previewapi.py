1901f1f59a27b6fa8c965ee39e218a30b8a56b1b
MaxMagician <43075724+xiangweizheng@users.noreply.github.com>
2025-06-26 21:14:37 -0700

    feat(style): update global color scheme to black and orange

--- vibesgram/test/previewapi.py
+++ vibesgram/test/previewapi.py
@@ -1,11 +1,11 @@
 import requests
 
 # API地址，根据实际情况修改
-url = "http://localhost:3000/api/v1/preview/create"
-
+# url = "http://localhost:3000/api/v1/preview/create"
+url = "http://www.binbody.com/api/v1/preview/create"
 # 构造请求体
 payload = {
-    "html": "<h1>v1 hello</h1>"
+    "html": "<h1>v1 hello prod</h1>"
 }
 
 # 发送POST请求
